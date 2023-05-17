0215f5b862220e85ef5fcba4d3fb89a4fbb52750
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-17 12:36:06 -0600

    Update Cliente.java
    Actualiación de POJO con respecto a BD

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Cliente.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Cliente.java
@@ -1,22 +1,48 @@
 package javafxsgemec.pojo;
 
 public class Cliente {
+    private int idCliente;
     private String nombre;
     private String apellidoPaterno;
     private String apellidoMaterno;
+    private String direccion;
     private long telefono;
+    private String correoElectronico;
+    private int idUsuario;
+    private String nombreUsuario;
+    private String password;
+    private int idRoles;
+    private String nivelAcceso;
     
     public Cliente(){
         
     }
     
-    public Cliente(String nombre, String apellidoPaterno, String apellidoMaterno, long telefono) {
+    public Cliente(int idCliente, String nombre, String apellidoPaterno, String apellidoMaterno, 
+                   String direccion, long telefono, String correoElectronio, int idUsuario, 
+                   String nombreUsuario, String password, int idRoles, String nivelAcceso) {
+        this.idCliente = idCliente;
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
+        this.direccion = direccion;
         this.telefono = telefono;
+        this.correoElectronico = correoElectronio;
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.idRoles = idRoles;
+        this.nivelAcceso = nivelAcceso;
     }
 
+    public void setIdCliente(int idCliente){
+        this.idCliente = idCliente;
+    }
+    
+    public int getIdCliente(){
+        return this.idCliente;
+    }
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -41,12 +67,68 @@ public class Cliente {
         return this.apellidoMaterno;
     }
     
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+    
     public void setTelefono(long telefono) {
         this.telefono = telefono;
     }
 
     public long getTelefono() {
         return this.telefono;
+    }
+    
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getCorreoElectronico() {
+        return this.correoElectronico;
+    }
+    
+    public void setIdUsuario(int idUsuario){
+        this.idUsuario = idUsuario;
+    }
+    
+    public int getIdUsuario(){
+        return this.idUsuario;
+    }
+    
+    public void setNombreUsuario(String nombreUsuario){
+        this.nombreUsuario = nombreUsuario;
+    }
+        
+    public String nombreUsuario(){
+        return this.nombreUsuario;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setIdRoles(int idRoles){
+        this.idRoles = idRoles;
+    }
+        
+    public int getIdRoles(){
+        return this.idRoles;
+    }
+    
+    public void setNivelAcceso(String nivelAcceso){
+        this.nivelAcceso = nivelAcceso;
+    }
+        
+    public String getNivelAcceso(){
+        return this.nivelAcceso;
     }
     
     @Override
