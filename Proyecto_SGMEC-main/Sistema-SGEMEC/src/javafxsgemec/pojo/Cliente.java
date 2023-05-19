b9a8ba6efbf61df98fcbdeb330db472c1fcde466
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-19 09:40:09 -0600

    Update Cliente.java

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Cliente.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Cliente.java
@@ -1,138 +1,82 @@
 package javafxsgemec.pojo;
 
-public class Cliente {
-    private int idCliente;
-    private String nombre;
-    private String apellidoPaterno;
-    private String apellidoMaterno;
-    private String direccion;
-    private long telefono;
-    private String correoElectronico;
-    private int idUsuario;
-    private String nombreUsuario;
-    private String password;
-    private int idRoles;
-    private String nivelAcceso;
-    
+public class Cliente {  
     public Cliente(){
         
     }
     
-    public Cliente(int idCliente, String nombre, String apellidoPaterno, String apellidoMaterno, 
-                   String direccion, long telefono, String correoElectronio, int idUsuario, 
-                   String nombreUsuario, String password, int idRoles, String nivelAcceso) {
-        this.idCliente = idCliente;
-        this.nombre = nombre;
-        this.apellidoPaterno = apellidoPaterno;
-        this.apellidoMaterno = apellidoMaterno;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.correoElectronico = correoElectronio;
-        this.idUsuario = idUsuario;
-        this.nombreUsuario = nombreUsuario;
-        this.password = password;
-        this.idRoles = idRoles;
-        this.nivelAcceso = nivelAcceso;
+    public Cliente() {
     }
 
-    public void setIdCliente(int idCliente){
-        this.idCliente = idCliente;
+    public void setIdCliente(){
     }
     
     public int getIdCliente(){
-        return this.idCliente;
     }
     
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre() {
     }
 
     public String getNombre() {
-        return this.nombre;
     }
 
-    public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
+    public void setApellidoPaterno() {
     }
 
     public String getApellidoPaterno() {
-        return this.apellidoPaterno;
     }
 
-    public void setApellidoMaterno(String apellidoMaterno) {
-        this.apellidoMaterno = apellidoMaterno;
+    public void setApellidoMaterno() {
     }
 
     public String getApellidoMaterno() {
-        return this.apellidoMaterno;
     }
     
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
+    public void setDireccion() {
     }
 
-    public String getDireccion() {
-        return this.direccion;
+    public String getDireccion() {        
     }
     
-    public void setTelefono(long telefono) {
-        this.telefono = telefono;
+    public void setTelefono() {
     }
 
     public long getTelefono() {
-        return this.telefono;
     }
     
-    public void setCorreoElectronico(String correoElectronico) {
-        this.correoElectronico = correoElectronico;
+    public void setCorreoElectronico() {
     }
 
     public String getCorreoElectronico() {
-        return this.correoElectronico;
     }
     
-    public void setIdUsuario(int idUsuario){
-        this.idUsuario = idUsuario;
+    public void setIdUsuario(){
     }
     
     public int getIdUsuario(){
-        return this.idUsuario;
     }
     
-    public void setNombreUsuario(String nombreUsuario){
-        this.nombreUsuario = nombreUsuario;
+    public void setNombreUsuario(){
     }
         
     public String nombreUsuario(){
-        return this.nombreUsuario;
     }
     
-    public void setPassword(String password){
-        this.password = password;
+    public void setPassword(){
     }
     
     public String getPassword(){
-        return this.password;
     }
 
-    public void setIdRoles(int idRoles){
-        this.idRoles = idRoles;
+    public void setIdRoles(){
     }
         
     public int getIdRoles(){
-        return this.idRoles;
     }
     
     public void setNivelAcceso(String nivelAcceso){
-        this.nivelAcceso = nivelAcceso;
     }
         
     public String getNivelAcceso(){
-        return this.nivelAcceso;
-    }
-    
-    @Override
-    public String toString(){
-        return nombre+" "+apellidoPaterno+" "+apellidoMaterno;
     }
 }
