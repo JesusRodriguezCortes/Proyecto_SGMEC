8c7e68ddb5e5d9260c9478cc1dfc67f606888fbf
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-19 09:45:40 -0600

    Update Rol.java

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Rol.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Rol.java
@@ -1,36 +1,9 @@
 package javafxsgemec.pojo;
 
 public class Rol {
-    private int idRoles;
-    private String nivelAcceso;
-
     public Rol(){
         
     }
     
-    public Rol(int idRoles, String nivelAcceso) {
-        this.idRoles = idRoles;
-        this.nivelAcceso = nivelAcceso;
-    }
-
-    public int getIdRoles() {
-        return idRoles;
-    }
-
-    public void setIdRoles(int idRoles) {
-        this.idRoles = idRoles;
-    }
-
-    public String getNivelAcceso() {
-        return nivelAcceso;
-    }
-
-    public void setNivelAcceso(String nivelAcceso) {
-        this.nivelAcceso = nivelAcceso;
-    }
-    
-    @Override
-    public String toString(){
-        return this.nivelAcceso;
-    }
+    //SETTERS Y GETTERS
 }
