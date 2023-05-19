6d4cd735d0de9c05a6f160bff71dddae5ce301c5
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-18 19:54:32 -0600

    Update Servicio.java
    POJO actualizado con el DAO

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Servicio.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/Servicio.java
@@ -1,35 +1,36 @@
 package javafxsgemec.pojo;
 
 public class Servicio {
-    private float precio;
-    private String tipoServicio;
+    private int idServicio;
+    private String nombre;
     
     public Servicio(){
+        
     }
 
-    public Servicio(float precio, String tipoServicio) {
-        this.precio = precio;
-        this.tipoServicio = tipoServicio;
+    public Servicio(int idServicio, String nombre) {
+        this.idServicio = idServicio;
+        this.nombre = nombre;
     }
 
-    public void setPrecio(float precio) {
-        this.precio = precio;
+    public int getIdServicio() {
+        return idServicio;
     }
 
-    public float getPrecio(){
-        return this.precio;
+    public void setIdServicio(int idServicio) {
+        this.idServicio = idServicio;
     }
-    
-    public void setTipoServicio(String tipoServicio) {
-        this.tipoServicio = tipoServicio;
+
+    public String getNombre() {
+        return nombre;
     }
-    
-    public String getTipoServicio(){
-        return this.tipoServicio;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     
     @Override
     public String toString(){
-        return this.tipoServicio+" "+this.precio;
+        return this.nombre;
     }
 }
