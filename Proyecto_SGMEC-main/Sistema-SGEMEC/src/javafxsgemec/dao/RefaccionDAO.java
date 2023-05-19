15055ab7db18f40486b28e18b9dcf6d5f3d42581
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-18 21:58:00 -0600

    Update RefaccionDAO.java

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/dao/RefaccionDAO.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/dao/RefaccionDAO.java
@@ -1,17 +1,12 @@
 package javafxsgemec.dao;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafxsgemec.pojo.Refaccion;
 import javafxsgemec.connectionBD.OpenConnection;
-import javafxsgemec.connectionBD.ConstantsConnection;
 import javafxsgemec.connectionBD.ResultOperation;
 
 public class RefaccionDAO {
@@ -27,15 +22,15 @@ public class RefaccionDAO {
                                   "pzasDisponiblesVenta, precioVenta, precioCompra, idProveedor, idDiagnostico) " +
                                   "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
                 PreparedStatement getCliente = conexionBD.prepareStatement(sqlQuery);
-                getCliente.setString(1, newRefaccion.getNombre());
-                getCliente.setString(2, newRefaccion.getTipoRefaccion());
-                getCliente.setString(3, newRefaccion.getSKU());
-                getCliente.setInt(4, newRefaccion.getPzsCompra());
-                getCliente.setInt(5, newRefaccion.getPzsVenta());
-                getCliente.setFloat(6, newRefaccion.getPrecioCompra());
-                getCliente.setFloat(7, newRefaccion.getPzsVenta());
-                getCliente.setFloat(8, newRefaccion.getIdProveedor());
-                getCliente.setFloat(9, newRefaccion.getIdDiagnostico());
+                    getCliente.setString(1, newRefaccion.getNombre());
+                    getCliente.setString(2, newRefaccion.getTipoRefaccion());
+                    getCliente.setString(3, newRefaccion.getSKU());
+                    getCliente.setInt(4, newRefaccion.getPzsCompra());
+                    getCliente.setInt(5, newRefaccion.getPzsVenta());
+                    getCliente.setFloat(6, newRefaccion.getPrecioCompra());
+                    getCliente.setFloat(7, newRefaccion.getPzsVenta());
+                    getCliente.setFloat(8, newRefaccion.getIdProveedor());
+                    getCliente.setFloat(9, newRefaccion.getIdDiagnostico());
                 
                 int affectedRows = getCliente.executeUpdate();
                 if(affectedRows > 0){ 
@@ -65,28 +60,28 @@ public class RefaccionDAO {
         
         if(conexionBD != null){
             try {
-                String sqlQuery = "SELECT refaccion.idRefaccion as 'idRefaccion', refaccion.nombre as 'nombreRefaccion', tipoRefaccion, " +
-                                  "codifoRefaccion, pzasDisponiblesCompra, precioCompra, pzasDisponiblesVenta, precioVenta, " +
-                                  "proveedor.idProveedor, proveedor.nombre as 'nombreProveedor', diagnostico.idDiagnostico, " +
-                                  "diagnostico.fechaFin " +
+                String sqlQuery = "SELECT refaccion.idRefaccion as 'idRefaccion', refaccion.nombre as 'nombreRefaccion', " +
+                                  "tipoRefaccion, codifoRefaccion, pzasDisponiblesCompra, precioCompra, pzasDisponiblesVenta, " +
+                                  "precioVenta, proveedor.idProveedor, proveedor.nombre as 'nombreProveedor', " +
+                                  "diagnostico.idDiagnostico, diagnostico.fechaFin " +
                                   "FROM refaccion LEFT JOIN proveedor ON refaccion.idProveedor = proveedor.idProveedor " +
-                                  "LEFT JOIN diagnostico ON diagnostico.idDiagnostico" +
+                                  "LEFT JOIN diagnostico ON diagnostico.idDiagnostico " +
                                   "WHERE refaccion.idRefaccion = ?";
                 PreparedStatement getRefaccion = conexionBD.prepareStatement(sqlQuery);
                 ResultSet resultSet = getRefaccion.executeQuery();
                 getRefaccion.setInt(1, idRefaccion);
-                refaccionBD.setIdRefaccion(resultSet.getInt("idRefaccion"));
-                refaccionBD.setNombre(resultSet.getString("nombreRefaccion"));
-                refaccionBD.setTipoRefaccion(resultSet.getString("tipoRefaccion"));
-                refaccionBD.setSKU(resultSet.getString("codifoRefaccion"));
-                refaccionBD.setPzsCompra(resultSet.getInt("pzasDisponiblesCompra"));
-                refaccionBD.setPrecioCompra(resultSet.getFloat("precioCompra"));
-                refaccionBD.setPzsVenta(resultSet.getInt("pzasDisponiblesVenta"));
-                refaccionBD.setPrecioVenta(resultSet.getFloat("precioVenta"));
-                refaccionBD.setIdProveedor(resultSet.getInt("'idProveedor'"));
-                refaccionBD.setNombreProveedor(resultSet.getString("nombreProveedor"));
-                refaccionBD.setIdDiagnostico(resultSet.getInt("idDiagnostico"));
-                refaccionBD.setFechaFin(resultSet.getString("fechaFin"));
+                    refaccionBD.setIdRefaccion(resultSet.getInt("idRefaccion"));
+                    refaccionBD.setNombre(resultSet.getString("nombreRefaccion"));
+                    refaccionBD.setTipoRefaccion(resultSet.getString("tipoRefaccion"));
+                    refaccionBD.setSKU(resultSet.getString("codifoRefaccion"));
+                    refaccionBD.setPzsCompra(resultSet.getInt("pzasDisponiblesCompra"));
+                    refaccionBD.setPrecioCompra(resultSet.getFloat("precioCompra"));
+                    refaccionBD.setPzsVenta(resultSet.getInt("pzasDisponiblesVenta"));
+                    refaccionBD.setPrecioVenta(resultSet.getFloat("precioVenta"));
+                    refaccionBD.setIdProveedor(resultSet.getInt("'idProveedor'"));
+                    refaccionBD.setNombreProveedor(resultSet.getString("nombreProveedor"));
+                    refaccionBD.setIdDiagnostico(resultSet.getInt("idDiagnostico"));
+                    refaccionBD.setFechaFin(resultSet.getString("fechaFin"));
             } catch (SQLException e) {
                 response.setMessage(e.getMessage());
             } catch(NullPointerException f) {
@@ -100,7 +95,7 @@ public class RefaccionDAO {
         return refaccionBD;
     }
     
-    public static List<Refaccion> getRefacciones() throws SQLException{
+    public static ArrayList<Refaccion> getRefacciones() throws SQLException{
         ArrayList<Refaccion> RefaccionesBD = null;
         ResultOperation response = new ResultOperation();
         response.setError(true);
@@ -120,18 +115,18 @@ public class RefaccionDAO {
                 RefaccionesBD = new ArrayList<>();
                 while(resultSet.next()){
                     Refaccion newRefaccion = new Refaccion();
-                    newRefaccion.setIdRefaccion(resultSet.getInt("idRefaccion"));
-                    newRefaccion.setNombre(resultSet.getString("nombreRefaccion"));
-                    newRefaccion.setTipoRefaccion(resultSet.getString("tipoRefaccion"));
-                    newRefaccion.setSKU(resultSet.getString("codigoRefaccion"));
-                    newRefaccion.setPzsCompra(resultSet.getInt("pzasDisponiblesCompra"));
-                    newRefaccion.setPrecioCompra(resultSet.getFloat("precioCompra"));
-                    newRefaccion.setPzsVenta(resultSet.getInt("pzasDisponiblesVenta"));
-                    newRefaccion.setPrecioVenta(resultSet.getFloat("precioVenta"));
-                    newRefaccion.setIdProveedor(resultSet.getInt("'idProveedor'"));
-                    newRefaccion.setNombreProveedor(resultSet.getString("nombreProveedor"));
-                    newRefaccion.setIdDiagnostico(resultSet.getInt("idDiagnostico"));
-                    newRefaccion.setFechaFin(resultSet.getString("fechaFin"));
+                        newRefaccion.setIdRefaccion(resultSet.getInt("idRefaccion"));
+                        newRefaccion.setNombre(resultSet.getString("nombreRefaccion"));
+                        newRefaccion.setTipoRefaccion(resultSet.getString("tipoRefaccion"));
+                        newRefaccion.setSKU(resultSet.getString("codigoRefaccion"));
+                        newRefaccion.setPzsCompra(resultSet.getInt("pzasDisponiblesCompra"));
+                        newRefaccion.setPrecioCompra(resultSet.getFloat("precioCompra"));
+                        newRefaccion.setPzsVenta(resultSet.getInt("pzasDisponiblesVenta"));
+                        newRefaccion.setPrecioVenta(resultSet.getFloat("precioVenta"));
+                        newRefaccion.setIdProveedor(resultSet.getInt("'idProveedor'"));
+                        newRefaccion.setNombreProveedor(resultSet.getString("nombreProveedor"));
+                        newRefaccion.setIdDiagnostico(resultSet.getInt("idDiagnostico"));
+                        newRefaccion.setFechaFin(resultSet.getString("fechaFin"));
                     RefaccionesBD.add(newRefaccion);
                 }
             } catch (SQLException e) {
@@ -160,16 +155,16 @@ public class RefaccionDAO {
                                   "precioCompra = ?, idProveedor = ?, idDiagnostico = ?" +
                                   "WHERE idRefaccion = ?";
                 PreparedStatement setRefaccion = conexionBD.prepareStatement(sqlQuery);
-                setRefaccion.setString(1, editRefaccion.getNombre());
-                setRefaccion.setString(2, editRefaccion.getTipoRefaccion());
-                setRefaccion.setString(3, editRefaccion.getSKU());
-                setRefaccion.setInt(4, editRefaccion.getPzsCompra());
-                setRefaccion.setInt(5, editRefaccion.getPzsVenta());
-                setRefaccion.setFloat(6, editRefaccion.getPrecioVenta());
-                setRefaccion.setFloat(7, editRefaccion.getPrecioCompra());
-                setRefaccion.setInt(8, editRefaccion.getIdProveedor());
-                setRefaccion.setInt(9, editRefaccion.getIdDiagnostico());
-                setRefaccion.setInt(10, editRefaccion.getIdRefaccion());
+                    setRefaccion.setString(1, editRefaccion.getNombre());
+                    setRefaccion.setString(2, editRefaccion.getTipoRefaccion());
+                    setRefaccion.setString(3, editRefaccion.getSKU());
+                    setRefaccion.setInt(4, editRefaccion.getPzsCompra());
+                    setRefaccion.setInt(5, editRefaccion.getPzsVenta());
+                    setRefaccion.setFloat(6, editRefaccion.getPrecioVenta());
+                    setRefaccion.setFloat(7, editRefaccion.getPrecioCompra());
+                    setRefaccion.setInt(8, editRefaccion.getIdProveedor());
+                    setRefaccion.setInt(9, editRefaccion.getIdDiagnostico());
+                    setRefaccion.setInt(10, editRefaccion.getIdRefaccion());
                 
                 int rowsAffected = setRefaccion.executeUpdate();
                 if(rowsAffected > 0){
