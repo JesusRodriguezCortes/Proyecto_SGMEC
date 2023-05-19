d0e86ab636b94a3bb06eab46e8aaec6dd000c67c
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-19 09:45:17 -0600

    Update RefaccionComprada.java

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/RefaccionComprada.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/pojo/RefaccionComprada.java
@@ -1,41 +1,9 @@
 package javafxsgemec.pojo;
 
 public class RefaccionComprada {
-    private int idRefaccComprada;
-    private int idPedidoRefacc;
-    private int idRefaccion;
-    
     public RefaccionComprada(){
         
     }
 
-    public RefaccionComprada(int idRefaccComprada, int idPedidoRefacc, int idRefaccion) {
-        this.idRefaccComprada = idRefaccComprada;
-        this.idPedidoRefacc = idPedidoRefacc;
-        this.idRefaccion = idRefaccion;
-    }
-
-    public int getIdRefaccComprada() {
-        return idRefaccComprada;
-    }
-
-    public void setIdRefaccComprada(int idRefaccComprada) {
-        this.idRefaccComprada = idRefaccComprada;
-    }
-
-    public int getIdPedidoRefacc() {
-        return idPedidoRefacc;
-    }
-
-    public void setIdPedidoRefacc(int idPedidoRefacc) {
-        this.idPedidoRefacc = idPedidoRefacc;
-    }
-
-    public int getIdRefaccion() {
-        return idRefaccion;
-    }
-
-    public void setIdRefaccion(int idRefaccion) {
-        this.idRefaccion = idRefaccion;
-    }
+    //SETTERS Y GETTERS
 }
