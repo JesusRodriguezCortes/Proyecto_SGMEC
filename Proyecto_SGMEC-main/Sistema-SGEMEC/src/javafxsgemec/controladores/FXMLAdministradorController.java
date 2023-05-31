2c96f7ba7bd4a33e9857c1f8ac50dffdf1190a81
Jesus_Rodriguez <85371341+JesusRodriguezCortes@users.noreply.github.com>
2023-05-30 19:09:45 -0600

    Preparativos para el segundo Merge

--- Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/controladores/FXMLAdministradorController.java
+++ Proyecto_SGMEC-main/Sistema-SGEMEC/src/javafxsgemec/controladores/FXMLAdministradorController.java
@@ -1,20 +1,8 @@
 package javafxsgemec.controladores;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafxsgemec.javafxsgemec;
 
 /**
  * FXML Controller class
@@ -23,8 +11,6 @@ import javafxsgemec.javafxsgemec;
  */
 public class FXMLAdministradorController implements Initializable {
 
-    @FXML
-    private Button btnPedidos;
 
     /**
      * Initializes the controller class.
@@ -34,18 +20,5 @@ public class FXMLAdministradorController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void btnRealizarPedido(ActionEvent event) {
-        try {
-           Parent vista = FXMLLoader.load(javafxsgemec.class.getResource("vistas/FXMLSolicitudRefacciones.fxml"));
-           Scene escenaPrincipal = new Scene(vista);
-           Stage escenarioBase = (Stage) btnPedidos.getScene().getWindow();
-           escenarioBase.setScene(escenaPrincipal);
-           escenarioBase.show(); 
-        } catch (IOException ex) {
-            Logger.getLogger(FXMLAdministradorController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
-    }
     
 }
